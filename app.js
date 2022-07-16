fdfb81c15d5328c36b283a2b773e83e8953e94ae
srishty398 <102925186+srishty398@users.noreply.github.com>
2022-07-16 19:27:26 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const feedDisplay = document.querySelector('#feed');
-
-http://localhost:3001/accounts/
-
-fetch('http://localhost:3001/account')
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(err => console.log(err))
